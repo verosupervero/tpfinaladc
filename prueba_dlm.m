1695b06033aecdee24c091043bbd9ac71502f708
verosupervero <verobustamante95@gmail.com>
2019-09-12 19:35:43 -0300

    prub dlm

---
+++ prueba_dlm.m
@@ -0,0 +1,3 @@
+leido = dlmread('/home/vero/Documentos/ADC/tp/respuesta_escalon.txt',',')
+leido2 = dlmread('/home/vero/Documentos/ADC/tp/img/mediciones/mediciones.csv',',')
+leido3 = csvread('/home/vero/Documentos/ADC/tp/img/mediciones/mediciones.csv')
